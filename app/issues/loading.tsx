3c3783a77889168506bd342a4f8d36663482ff88
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-19 22:04:03 +0530

    Add IssueActions component and loading skeleton

---
+++ app/issues/loading.tsx
@@ -0,0 +1,49 @@
+import { Button, Callout, Skeleton, Table, Text, TextArea, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import "easymde/dist/easymde.min.css";
+import {Controller, useForm} from 'react-hook-form'
+
+import SimpleMDE from 'react-simplemde-editor';
+import { useRouter } from 'next/navigation';
+import IssueActions from './IssueActions';
+
+
+const loading = () => {
+
+  return (
+    <div>
+      <IssueActions/>
+         <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {[1,2,3,4,5,6].map((issue) => (
+            <Table.Row key={""}>
+              <Table.Cell>
+              <Skeleton/>
+
+                <div className="block md:hidden">
+              <Skeleton/>
+                </div>
+              </Table.Cell>
+              <Table.Cell>
+                <Skeleton/>
+                <div className="hidden md:table-cell">
+                  <Skeleton/>
+                </div>
+              </Table.Cell>
+              <Table.Cell><Skeleton/></Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  )
+}
+
+export default loading
