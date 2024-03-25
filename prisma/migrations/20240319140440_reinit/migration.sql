4237638447c501d28c012bb04e1bef9a928b997b
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-25 22:52:34 +0530

    Delete migration files and update database schema

--- prisma/migrations/20240319140440_reinit/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status` on the `Issue` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(0))` to `Enum(EnumId(0))`.
-
-*/
--- AlterTable
-ALTER TABLE `Issue` MODIFY `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN';
