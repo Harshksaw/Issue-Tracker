2ce990d1c16fef80eef886a1c19b51e0ef081cd9
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-27 19:26:09 +0530

    Add Sentry configuration files and update dependencies

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -13,18 +13,23 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "@/app/api/issues/[id]/AssigneeSelect";
+import { cache } from "react";
 interface Props {
   params: { id: string };
 }
+
+const fetchUser = cache((issueId: number)=>  prisma.issue.findUnique({where: {id:issueId} }) );
+
 
 const IssueDetailPage = async ({ params }: Props) => {
   // if (typeof params?.id !== 'number') notFound();
 
   const session = await getServerSession(authOptions);
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  // const issue = await prisma.issue.findUnique({
+  //   where: { id: parseInt(params.id) },
+  // });-->>
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) notFound();
 
@@ -50,9 +55,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({params} : Props){
-  const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
-  });
+  const issue =await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4e6389c6164abdf1ecf67cb7259cbef9@o4506983239647232.ingest.us.sentry.io/4506983246069760",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
